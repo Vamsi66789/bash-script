c78212e004181462c6a15baceb3c21222c3ed365
Vamsi66789 <141404338+Vamsi66789@users.noreply.github.com>
2024-06-13 16:35:52 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
-sudo apt update
-sudo apt-get full-upgrade -y
-sudo apt-get install -y python3-pip
+sudo yum update
+sudo yum-get full-upgrade -y
+sudo yum-get install -y python3-pip
 sudo git clone https://github.com/ajay77777777/flask-library-app.git
 cd flask-library-app/
 pip3 install -r requirements.txt
